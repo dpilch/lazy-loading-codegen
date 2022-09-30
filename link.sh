44435e983afc34b1810da8ccf164b986292a615d
Dane Pilcher <dppilche@amazon.com>
2022-09-30 11:11:56 -0600

    add eager/lazy change to unlink/link

--- link.sh
+++ link.sh
@@ -1,5 +1,5 @@
-(cd cra && npm link aws-amplify @aws-amplify/core @aws-amplify/datastore @aws-amplify/auth @aws-amplify/ui-components @aws-amplify/api @aws-amplify/api-graphql)
+(cd cra && npm link aws-amplify @aws-amplify/core @aws-amplify/datastore @aws-amplify/auth @aws-amplify/ui-components @aws-amplify/api @aws-amplify/api-graphql && cp LazyApp.tsx src/App.tsx)
 (cd next && npm link aws-amplify @aws-amplify/core @aws-amplify/datastore @aws-amplify/auth @aws-amplify/ui-components @aws-amplify/api @aws-amplify/api-graphql)
-(cd no-cra && npm link aws-amplify @aws-amplify/core @aws-amplify/datastore @aws-amplify/auth @aws-amplify/ui-components @aws-amplify/api @aws-amplify/api-graphql)
-(cd no-react && npm link aws-amplify @aws-amplify/core @aws-amplify/datastore @aws-amplify/auth @aws-amplify/ui-components @aws-amplify/api @aws-amplify/api-graphql)
+(cd no-cra && npm link aws-amplify @aws-amplify/core @aws-amplify/datastore @aws-amplify/auth @aws-amplify/ui-components @aws-amplify/api @aws-amplify/api-graphql && cp LazyApp.tsx src/App.tsx)
+(cd no-react && npm link aws-amplify @aws-amplify/core @aws-amplify/datastore @aws-amplify/auth @aws-amplify/ui-components @aws-amplify/api @aws-amplify/api-graphql && cp lazyindex.ts src/index.ts)
 (cd no-typescript && npm link aws-amplify @aws-amplify/core @aws-amplify/datastore @aws-amplify/auth @aws-amplify/ui-components @aws-amplify/api @aws-amplify/api-graphql)
