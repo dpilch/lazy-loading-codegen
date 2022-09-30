800b9a33a7dbe9b04a182392f01995450c16e7d1
Dane Pilcher <dppilche@amazon.com>
2022-09-30 11:47:03 -0600

    fix build

--- next/lazyindex.tsx
+++ next/lazyindex.tsx
@@ -1,7 +1,8 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import { DataStore, AsyncCollection } from "aws-amplify";
+import { DataStore } from "aws-amplify";
+import { AsyncCollection } from "@aws-amplify/datastore";
 import { Blog, Post, Comment } from "../src/models";
 import styles from '../styles/Home.module.css'
 
