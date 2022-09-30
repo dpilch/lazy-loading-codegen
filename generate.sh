d4b61886bf1f3bbd7dab4b5c45356597493d11ad
Dane Pilcher <dppilche@amazon.com>
2022-09-30 09:46:16 -0600

    add generate script

---
+++ generate.sh
@@ -0,0 +1,5 @@
+(cd cra && amplify-dev codegen models)
+(cd next && amplify-dev codegen models)
+(cd no-cra && amplify-dev codegen models)
+(cd no-react && amplify-dev codegen models)
+(cd no-typescript && amplify-dev codegen models)
